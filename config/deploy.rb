3822d5c4c7d06ccb8d882fbdd3e60c8f688e2111
Nicolas Pineault <nicolas.pineault@gmail.com>
2015-07-14 21:56:06 -0400

    robots

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :repo_url, 'https://github.com/nicopino73/marieandme.git'
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 #set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
+set :keep_releases, 2
 
 namespace :deploy do
 
